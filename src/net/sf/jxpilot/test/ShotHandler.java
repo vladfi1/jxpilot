636cf1d61e7ac0e3698db6208e957efbba7a2a88
vladfi1 <vladfi1@fe37e0e8-d384-4f82-8245-2b7ac03aeabd>
2008-08-26 14:35:32 +0000

    Improved shot handling-now all shots are properly displayed.
    git-svn-id: https://svn.code.sf.net/p/jxpilot/svn/trunk@44 fe37e0e8-d384-4f82-8245-2b7ac03aeabd

--- src/net/sf/jxpilot/test/ShotHandler.java
+++ src/net/sf/jxpilot/test/ShotHandler.java
@@ -6,6 +6,26 @@ import java.awt.geom.*;
 
 /**
  * Class that holds shots as an ArrayList. Shots are held as a private inner class.
+ * Shots are represented by a type, and an x/y.
+ * 
+ * Types relative to ship
+ * 
+ * 26 = 0,0 
+ * 25 = -1,0
+ * 22 = 0,-1
+ * 21 = -1,-1
+ * 
+ * +1 is one to the right
+ * +4 is one up
+ * 
+ * 			28	29	30	31
+ * 
+ * 			24	25	26	26
+ * 				  S
+ * 			20	21	22	23
+ * 
+ * 			16	17	18	19
+ * 
  * @author vlad
  */
 public class ShotHandler extends java.util.ArrayList<ShotHandler.Shot>
@@ -56,7 +76,8 @@ public class ShotHandler extends java.util.ArrayList<ShotHandler.Shot>
 	
 	
 	/**
-	 * De-activates all the shots in this ArrayList.
+	 * De-activates all the shots in this ArrayList. This method only sets the size to 0,
+	 * it does not actually set the shots to inactive to save on time.
 	 */
 	public void clearShots()
 	{
@@ -70,39 +91,52 @@ public class ShotHandler extends java.util.ArrayList<ShotHandler.Shot>
 		size = 0;
 	}
 
-	
+	/**
+	 * Adds a shot to this ShotHolder.
+	 * @param type Describes the area where the shot is. An area is a 256x256 square.
+	 * 				Areas are positioned relative to the ship.
+	 * @param x The x position within the area.
+	 * @param y The y position within the area.
+	 */
 	public void addShot(int type, short x, short y)
 	{
 		if (size < super.size())
 		{
-			this.get(size).setShot(type, x, y, true);
+			this.get(size).setShot(type, x, y);
 		}
 		else
 		{
-			this.add(new Shot().setShot(type, x, y, true));
+			this.add(new Shot().setShot(type, x, y));
 			System.out.println("Increasing shots size!");
 		}
 		size++;
 	}
 	
+	//methods/variables for dealing with areas
+	/**
+	 * 256 = 1+max value of an unsigned byte
+	 */
+	static final int AREA_SIZE = 256;
+	static int getXArea(int type)
+	{
+		return type%4 - 2;
+	}
 	
+	static int getYArea(int type)
+	{
+		return type / 4 - 6;
+	}
 	/**
 	 * Simple class for storing a shot.
 	 * @author vlad
 	 */
 	private class Shot implements Drawable
 	{
-		
 		private int type;
 		/**
 		 * Unsigned byte.
 		 */
 		private short x,y;
-		
-		/**
-		 * Whether or not to draw the shot.
-		 */
-		private boolean active=false;
 		
 		public Shot setShot(int type, short x, short y)
 		{
@@ -112,33 +146,18 @@ public class ShotHandler extends java.util.ArrayList<ShotHandler.Shot>
 			return this;
 		}
 		
-		public Shot setShot(int type, short x, short y, boolean active)
-		{
-			setShot(type, x, y);
-			setActive(active);
-			return this;
-		}
-		
 		public int getType(){return type;}
 		public short getX(){return x;}
 		public short getY(){return y;}
-		public void setActive(boolean b){active = b;}
-		public boolean isActive(){return active;}
 		
 		public void paintDrawable(Graphics2D g2d)
 		{
-			if (!active) return;
-			
 			AffineTransform saved = g2d.getTransform();
 			
 			g2d.setColor(ENEMY_COLOR);
-			//g2d.rotate(heading, x, y);
-			g2d.translate(x+selfX, y+selfY);
+			g2d.translate(x+selfX+getXArea(type)*AREA_SIZE, y+selfY+getYArea(type)*AREA_SIZE);
 			
 			g2d.fill(shotShape);
-			//g2d.rotate(-heading);
-			//g2d.translate(-x,-y);
-			//g2d.rotate(-heading, -x, -y);
 			g2d.setTransform(saved);
 		}
 		
